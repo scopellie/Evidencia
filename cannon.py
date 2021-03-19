bfa0d37629677771e6d35b0eafbd34fd74a5e17d
Santiago <a00828421@itesm.mx>
2021-03-18 23:36:44 -0600

    reduced gravity

--- cannon.py
+++ cannon.py
@@ -60,7 +60,7 @@ def move():
 
     # Move the cannon shot
     if inside(ball):
-        speed.y -= 0.35
+        speed.y -= 0.20
         ball.move(speed)
 
     # Make a copy of the existing target list before redrawing
